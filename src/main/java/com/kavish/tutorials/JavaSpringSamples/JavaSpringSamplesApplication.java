75b4abec48b4dff1b517874e7ea90765a314ec4c
Kavish Rizvi <rizvikavish205.com>
2022-11-03 15:15:55 +0530

    Initial Commit

---
+++ src/main/java/com/kavish/tutorials/JavaSpringSamples/JavaSpringSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.kavish.tutorials.JavaSpringSamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kavish/tutorials/JavaSpringSamples/JavaSpringSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavish.tutorials.JavaSpringSamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
